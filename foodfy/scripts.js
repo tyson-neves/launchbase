e591b54c7d2607447c9f80390cf9ab1dddf32ff2
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-02-26 17:07:47 -0300

    upgrade modal

--- foodfy/scripts.js
+++ foodfy/scripts.js
@@ -3,10 +3,13 @@ const cards = document.querySelectorAll(".card");
 
 for(let card of cards) {
     card.addEventListener("click", function(){
+        // const fotoId = card.getAttribute("id");
         modalOverlay.classList.add("active")
+        // modalOverlay.querySelector("img").src = `assets/${fotoId}.png`;
     })
 }
 
 document.querySelector(".btn-close").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
 })
+
