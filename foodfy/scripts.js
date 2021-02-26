e523f57be0d75bf8b74a62bdbbb2f65e653ab8c6
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-02-26 19:48:34 -0300

    finalizando o o desafio 1

--- foodfy/scripts.js
+++ foodfy/scripts.js
@@ -1,11 +1,13 @@
 const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 
+
 for(let card of cards) {
     card.addEventListener("click", function(){
-        // const fotoId = card.getAttribute("id");
+        const fotoId = card.getAttribute("id");
         modalOverlay.classList.add("active")
-        // modalOverlay.querySelector("img").src = `assets/${fotoId}.png`;
+        modalOverlay.querySelector("img").src = `assets/${fotoId}.png`;
+        
     })
 }
 
@@ -13,3 +15,12 @@ document.querySelector(".btn-close").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
 })
 
+for (let card of cards) {
+    card.addEventListener("click", function(){
+    const titulo = card.querySelector(".card_description").innerHTML;
+    const creator = card.querySelector(".creator").innerHTML;
+
+    modalOverlay.querySelector("h4").innerHTML = `${titulo}`;
+    modalOverlay.querySelector("p").innerHTML = `${creator}`;
+    })
+}    
