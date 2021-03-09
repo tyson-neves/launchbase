88c4f4bf5e094b73772ce5413a798fc2207c6bda
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-03-09 17:14:24 -0300

    uptade

--- backend_init/server.js
+++ backend_init/server.js
@@ -9,11 +9,25 @@ server.use(express.static('public')); // caminho dos elementos estáticos
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape:false
 })
 
 server.get("/", function(req, res) {
-    return res.render("about");
+    const about = {
+        avatar_url:  'imagens/baixados.png',
+        name: "Pedro Neves",
+        role: "Front-End Developer",
+        description: 'Programador front-end e UX Design que busca melhorar a experiência do usuário e implementação da parte do design da aplicação. Objetivo é ser um desenvolvedor da <a href="https://ciandt.com/br/pt-br" target="_blank">CI&T.',
+        links: [ 
+            { name: 'Github', url:'https://github.com/tyson-neves'},
+            { name: 'Rocketseat', url:'https://app.rocketseat.com.br/me/pedro-neves-1567041448'},
+            { name: 'Linkedin', url:'https://www.linkedin.com/in/devpedro-neves/'},
+        ]
+    }
+
+
+    return res.render("about", {about: about});
 });
 
 server.get("/portfolio", function(req, res) {
